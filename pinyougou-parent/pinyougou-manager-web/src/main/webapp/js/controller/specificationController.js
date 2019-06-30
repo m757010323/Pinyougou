81153416b89448cee92f4f3701c9973dc603326c
habbyppy <35439037+habbyppy@users.noreply.github.com>
2019-06-30 08:39:59 +0800

    新增选项卡里面删除操作,现在js里面定义deleTableRow方法 进而在html里面删除按钮添加ng-click方法,其中参数索引用$index

--- pinyougou-parent/pinyougou-manager-web/src/main/webapp/js/controller/specificationController.js
+++ pinyougou-parent/pinyougou-manager-web/src/main/webapp/js/controller/specificationController.js
@@ -3,7 +3,13 @@ app.controller('specificationController' ,function($scope,$controller,specificat
 	
 	$controller('baseController',{$scope:$scope});//继承
 
-	$scope.entity={specificationOptionList:[]};//初始化entity以及specificationOptionList
+    //批量选项删除
+    $scope.deleTableRow=function(index){
+        $scope.entity.specificationOptionList.splice(index,1);//删除
+    }
+
+
+    $scope.entity={specificationOptionList:[]};//初始化entity以及specificationOptionList
 	//新增选项行
 	$scope.addTableRow =function(){
         $scope.entity.specificationOptionList.push({});
