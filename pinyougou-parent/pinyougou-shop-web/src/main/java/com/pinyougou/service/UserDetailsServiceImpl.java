1afa61a736d8401be677b67b5f9726ec51454192
m757010323 <757010323@qq.com>
2019-07-03 20:34:24 +0800

    商家登陆 连接数据库 使用商家的id和密码 1.在impl里面实现对用户状态的筛选,只让通过认证的商家登陆

--- pinyougou-parent/pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
+++ pinyougou-parent/pinyougou-shop-web/src/main/java/com/pinyougou/service/UserDetailsServiceImpl.java
@@ -14,11 +14,11 @@ import java.util.List;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-//    private SellerService sellerService;
-//
-//    public void setSellerService(SellerService sellerService) {
-//        this.sellerService = sellerService;
-//    }
+    private SellerService sellerService;
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -27,20 +27,17 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         grantedAuths.add(new SimpleGrantedAuthority("ROLE_SELLER"));
 
-//        TbSeller seller = sellerService.findOne(username);
-
-        return new User(username,"123456", grantedAuths);
-
-//        if(seller!=null){
-//            if(seller.getStatus().equals("1")){
-//                return new User(username,seller.getPassword(),grantedAuths);
-//            }else{
-//                return null;
-//            }
-//        }else{
-//            return null;
-//        }
+        TbSeller seller = sellerService.findOne(username);
 
 
+        if(seller!=null){
+            if(seller.getStatus().equals("1")){
+                return new User(username,seller.getPassword(),grantedAuths);
+            }else{
+                return null;
+            }
+        }else{
+        return null;
+        }
     }
 }
