91e8c649e573eed410be4d9c357808efa69b33df
m757010323 <757010323@qq.com>
2019-07-05 11:21:50 +0800

    商品添加 提取富文本中的描述

--- pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -53,12 +53,13 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService){
 
     //新增
     $scope.add=function(){
+        $scope.entity.goodsDesc.introduction=editor.html();
         goodsService.add($scope.entity).success(
             function(response){
                 if(response.success){
                     alert('保存成功');
                     $scope.entity={};
-
+                    editor.html("");//清空富文本编辑器
                 }else{
                     alert(response.message);
                 }
