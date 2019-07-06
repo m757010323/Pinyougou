8075101f59ac0df04fe101797555cd60bec050d8
m757010323 <757010323@qq.com>
2019-07-06 19:17:07 +0800

    生成sku列表 深克隆两次循环

--- pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
+++ pinyougou-parent/pinyougou-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -63,6 +63,30 @@ app.controller('goodsController' ,function($scope,$controller   ,goodsService,up
     });
 
 
+    //创建SKU列表
+    $scope.createItemList=function(){
+        $scope.entity.itemList=[{spec:{},price:0,num:99999,status:'0',isDefault:'0' } ];//初始
+        var items=  $scope.entity.goodsDesc.specificationItems;
+        for(var i=0;i< items.length;i++){
+            $scope.entity.itemList = addColumn( $scope.entity.itemList,items[i].attributeName,items[i].attributeValue );
+        }
+    };
+    //添加列值
+    addColumn=function(list,columnName,conlumnValues){
+        var newList=[];//新的集合
+        for(var i=0;i<list.length;i++){
+            var oldRow= list[i];
+            for(var j=0;j<conlumnValues.length;j++){
+                var newRow= JSON.parse( JSON.stringify( oldRow )  );//深克隆
+                newRow.spec[columnName]=conlumnValues[j];
+                newList.push(newRow);
+            }
+        }
+        return newList;
+    };
+
+
+
 
     /**
 	 * 上传图片
