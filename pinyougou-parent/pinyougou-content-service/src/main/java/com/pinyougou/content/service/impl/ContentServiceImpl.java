4a8442bedb092ae1ce5949c71a5d3a2c3d667db1
m757010323 <757010323@qq.com>
2019-07-10 17:05:03 +0800

    实现轮播图

--- pinyougou-parent/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou-parent/pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -105,5 +105,16 @@ public class ContentServiceImpl implements ContentService {
 		Page<TbContent> page= (Page<TbContent>)contentMapper.selectByExample(example);		
 		return new PageResult(page.getTotal(), page.getResult());
 	}
-	
+
+	@Override
+	public List<TbContent> findByCategoryId(Long categoryid) {
+		TbContentExample example = new TbContentExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andCategoryIdEqualTo(categoryid);
+		criteria.andStatusEqualTo("1");
+		example.setOrderByClause("sort_order");
+		return 	contentMapper.selectByExample(example);
+
+	}
+
 }
